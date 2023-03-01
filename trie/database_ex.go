45a1c2682467ed3b81ab08292f3f354453b137a9
lyh169 <liuyuhang203@sina.com>
2023-03-01 16:43:48 +0800

    add ac

---
+++ trie/database_ex.go
@@ -0,0 +1,248 @@
+package trie
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/log"
+	"time"
+)
+
+type keyvalue struct {
+	key    []byte
+	value  []byte
+	delete bool
+}
+
+type Cache struct {
+	dirties   map[common.Hash]*cachedNode
+	preimages map[common.Hash][]byte
+}
+
+func (b *BatchEx) PutDirties(key []byte, value []byte) error {
+}
+
+type BatchEx struct {
+	writes []keyvalue
+	size   int
+	data   chan []keyvalue
+}
+
+// Put inserts the given value into the key-value data store.
+func (b *BatchEx) Put(key []byte, value []byte) error {
+	b.writes = append(b.writes, keyvalue{common.CopyBytes(key), common.CopyBytes(value), false})
+	b.size += len(key) + len(value)
+	return nil
+}
+
+// Delete removes the key from the key-value data store.
+func (b *BatchEx) Delete(key []byte) error {
+	b.writes = append(b.writes, keyvalue{common.CopyBytes(key), nil, true})
+	b.size += len(key)
+	return nil
+}
+
+func (b *BatchEx) ValueSize() int {
+	return b.size
+}
+
+func (b *BatchEx) Reset() {
+	b.writes = b.writes[:0]
+	b.size = 0
+}
+
+func (b *BatchEx) Write() error {
+	b.data <- b.writes
+	return nil
+}
+
+func (b *BatchEx) Replay(w ethdb.KeyValueWriter) error {
+	for _, keyvalue := range b.writes {
+		if keyvalue.delete {
+			if err := w.Delete(keyvalue.key); err != nil {
+				return err
+			}
+			continue
+		}
+		if err := w.Put(keyvalue.key, keyvalue.value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Cap iteratively flushes old but still referenced trie nodes until the total
+// memory usage goes below the given threshold.
+//
+// Note, this method is a non-synchronized mutator. It is unsafe to call this
+// concurrently with other mutators.
+func (db *Database) CapEx(limit common.StorageSize) error {
+	// Create a database batch to flush persistent data out. It is important that
+	// outside code doesn't see an inconsistent state (referenced data removed from
+	// memory cache during commit but not yet in persistent storage). This is ensured
+	// by only uncaching existing data when the database write finalizes.
+	nodes, storage, start := len(db.dirties), db.dirtiesSize, time.Now()
+	batch := db.diskdb.NewBatch()
+
+	// db.dirtiesSize only contains the useful data in the cache, but when reporting
+	// the total memory consumption, the maintenance metadata is also needed to be
+	// counted.
+	size := db.dirtiesSize + common.StorageSize((len(db.dirties)-1)*cachedNodeSize)
+	size += db.childrenSize - common.StorageSize(len(db.dirties[common.Hash{}].children)*(common.HashLength+2))
+
+	// If the preimage cache got large enough, push to disk. If it's still small
+	// leave for later to deduplicate writes.
+	if db.preimages != nil {
+		db.preimages.commit(false)
+	}
+	// Keep committing nodes from the flush-list until we're below allowance
+	oldest := db.oldest
+	for size > limit && oldest != (common.Hash{}) {
+		// Fetch the oldest referenced node and push into the batch
+		node := db.dirties[oldest]
+		rawdb.WriteTrieNode(batch, oldest, node.rlp())
+
+		// If we exceeded the ideal batch size, commit and reset
+		if batch.ValueSize() >= ethdb.IdealBatchSize {
+			if err := batch.Write(); err != nil {
+				log.Error("Failed to write flush list to disk", "err", err)
+				return err
+			}
+			batch.Reset()
+		}
+		// Iterate to the next flush item, or abort if the size cap was achieved. Size
+		// is the total size, including the useful cached data (hash -> blob), the
+		// cache item metadata, as well as external children mappings.
+		size -= common.StorageSize(common.HashLength + int(node.size) + cachedNodeSize)
+		if node.children != nil {
+			size -= common.StorageSize(cachedNodeChildrenSize + len(node.children)*(common.HashLength+2))
+		}
+		oldest = node.flushNext
+	}
+	// Flush out any remainder data from the last batch
+	if err := batch.Write(); err != nil {
+		log.Error("Failed to write flush list to disk", "err", err)
+		return err
+	}
+	// Write successful, clear out the flushed data
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	for db.oldest != oldest {
+		node := db.dirties[db.oldest]
+		delete(db.dirties, db.oldest)
+		db.oldest = node.flushNext
+
+		db.dirtiesSize -= common.StorageSize(common.HashLength + int(node.size))
+		if node.children != nil {
+			db.childrenSize -= common.StorageSize(cachedNodeChildrenSize + len(node.children)*(common.HashLength+2))
+		}
+	}
+	if db.oldest != (common.Hash{}) {
+		db.dirties[db.oldest].flushPrev = common.Hash{}
+	}
+	db.flushnodes += uint64(nodes - len(db.dirties))
+	db.flushsize += storage - db.dirtiesSize
+	db.flushtime += time.Since(start)
+
+	memcacheFlushTimeTimer.Update(time.Since(start))
+	memcacheFlushSizeMeter.Mark(int64(storage - db.dirtiesSize))
+	memcacheFlushNodesMeter.Mark(int64(nodes - len(db.dirties)))
+
+	log.Debug("Persisted nodes from memory database", "nodes", nodes-len(db.dirties), "size", storage-db.dirtiesSize, "time", time.Since(start),
+		"flushnodes", db.flushnodes, "flushsize", db.flushsize, "flushtime", db.flushtime, "livenodes", len(db.dirties), "livesize", db.dirtiesSize)
+
+	return nil
+}
+
+// batch write 的时候将数据信息发送到后台channel，batch replay时候需要将内存中节点信息除了删除关系还需要放入到另外一个内存中以供使用
+
+// Commit iterates over all the children of a particular node, writes them out
+// to disk, forcefully tearing down all references in both directions. As a side
+// effect, all pre-images accumulated up to this point are also written.
+//
+// Note, this method is a non-synchronized mutator. It is unsafe to call this
+// concurrently with other mutators.
+func (db *Database) CommitEx(node common.Hash, report bool, callback func(common.Hash)) error {
+	// Create a database batch to flush persistent data out. It is important that
+	// outside code doesn't see an inconsistent state (referenced data removed from
+	// memory cache during commit but not yet in persistent storage). This is ensured
+	// by only uncaching existing data when the database write finalizes.
+	start := time.Now()
+	batch := db.diskdb.NewBatch()
+
+	// Move all of the accumulated preimages into a write batch
+	if db.preimages != nil {
+		db.preimages.commit(true)
+	}
+	// Move the trie itself into the batch, flushing if enough data is accumulated
+	nodes, storage := len(db.dirties), db.dirtiesSize
+
+	uncacher := &cleaner{db}
+	if err := db.commitEx(node, batch, uncacher, callback); err != nil {
+		log.Error("Failed to commit trie from trie database", "err", err)
+		return err
+	}
+	// Trie mostly committed to disk, flush any batch leftovers
+	if err := batch.Write(); err != nil {
+		log.Error("Failed to write trie to disk", "err", err)
+		return err
+	}
+	// Uncache any leftovers in the last batch
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	batch.Replay(uncacher)
+	batch.Reset()
+
+	// Reset the storage counters and bumped metrics
+	memcacheCommitTimeTimer.Update(time.Since(start))
+	memcacheCommitSizeMeter.Mark(int64(storage - db.dirtiesSize))
+	memcacheCommitNodesMeter.Mark(int64(nodes - len(db.dirties)))
+
+	logger := log.Info
+	if !report {
+		logger = log.Debug
+	}
+	logger("Persisted trie from memory database", "nodes", nodes-len(db.dirties)+int(db.flushnodes), "size", storage-db.dirtiesSize+db.flushsize, "time", time.Since(start)+db.flushtime,
+		"gcnodes", db.gcnodes, "gcsize", db.gcsize, "gctime", db.gctime, "livenodes", len(db.dirties), "livesize", db.dirtiesSize)
+
+	// Reset the garbage collection statistics
+	db.gcnodes, db.gcsize, db.gctime = 0, 0, 0
+	db.flushnodes, db.flushsize, db.flushtime = 0, 0, 0
+
+	return nil
+}
+
+// commit is the private locked version of Commit.
+func (db *Database) commitEx(hash common.Hash, batch ethdb.Batch, uncacher *cleaner, callback func(common.Hash)) error {
+	// If the node does not exist, it's a previously committed node
+	node, ok := db.dirties[hash]
+	if !ok {
+		return nil
+	}
+	var err error
+	node.forChilds(func(child common.Hash) {
+		if err == nil {
+			err = db.commitEx(child, batch, uncacher, callback)
+		}
+	})
+	if err != nil {
+		return err
+	}
+	// If we've reached an optimal batch size, commit and start over
+	rawdb.WriteTrieNode(batch, hash, node.rlp())
+	if callback != nil {
+		callback(hash)
+	}
+	if batch.ValueSize() >= ethdb.IdealBatchSize {
+		if err := batch.Write(); err != nil {
+			return err
+		}
+		db.lock.Lock()
+		batch.Replay(uncacher)
+		batch.Reset()
+		db.lock.Unlock()
+	}
+	return nil
+}
